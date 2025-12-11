69a37f58c713a1fd862f61100ead60eaf5b58229
Nick Choate <nickchoate@berkeley.edu>
2025-12-11 11:31:02 -0800

    clean up and level up

--- server.js
+++ server.js
@@ -1,36 +1,28 @@
-const dotenv = require("dotenv")
-dotenv.config()
-const express = require("express")
-const app = express()
-
-const mongoose = require("mongoose")
-const methodOverride = require("method-override")
-const morgan = require("morgan")
-const session = require('express-session')
-const MongoStore = require("connect-mongo").default
-const isSignedIn = require("./middleware/is-signed-in.js")
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+const MongoStore = require("connect-mongo").default;
+const isSignedIn = require("./middleware/is-signed-in.js");
 const passUserToView = require("./middleware/pass-user-to-view.js");
 
-// Set the port from environment variable or default to 3000
-const port = process.env.PORT ? process.env.PORT : "3001"
+const authController = require('./controllers/auth.js');
 
-const authController = require("./controllers/auth.js")
+const port = process.env.PORT ? process.env.PORT : '3000';
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-// Middleware to parse URL-encoded data from forms
-app.use(express.urlencoded({ extended: false }))
-// Middleware for using HTTP verbs such as PUT or DELETE
-app.use(methodOverride("_method"))
-// Morgan for logging HTTP requests
-app.use(morgan('dev'))
-
-app.use(methodOverride("_method"))
-app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+// app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -44,20 +36,15 @@ app.use(
 
 app.use(passUserToView);
 
-
-
-// GET /
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.use("/auth", authController)
-
-app.get("/vip-lounge", (req, res) => {
+app.get('/vip-lounge', (req, res) => {
   if (req.session.user) {
     res.send(`Welcome to the party ${req.session.user.username}.`);
   } else {
-    res.send("Sorry, no guests allowed.");
+    res.send('Sorry, no guests allowed.');
   }
 });
 
@@ -65,8 +52,8 @@ app.get("/vip-lounge", isSignedIn, (req, res) => {
   res.send(`Welcome to the party ${req.session.user.username}.`);
 });
 
-
+app.use('/auth', authController);
 
 app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`)
-})
+  console.log(`The express app is ready on port ${port}!`);
+});
